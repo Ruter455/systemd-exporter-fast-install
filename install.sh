ce5ea084987a4124bc96a5131b8093a1732d412e
Ruter455 <alexey.telepin2003@gmail.com>
2023-11-26 13:09:24 +0600

    fix permission denied

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo cp systemd_exporter /usr/bin
 sudo cp systemdexporter.service /etc/systemd/system
+sudo chmod 755 /usr/bin/systemd_exporter
 sudo systemctl enable systemdexporter
 sudo systemctl start systemdexporter
