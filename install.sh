d02cecc85910b908150bf90634410fa4dfd5a0d1
Ruter455 <alexey.telepin2003@gmail.com>
2023-11-29 20:46:40 +0600

    node-exporter megrate

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
-sudo cp systemd_exporter /usr/bin
-sudo cp systemdexporter.service /etc/systemd/system
-sudo chmod 755 /usr/bin/systemd_exporter
+
+curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest| grep browser_download_url|grep linux-amd64|cut -d '"' -f 4|wget -qi -
+
+tar -xvf node_exporter*.tar.gz
+cd  node_exporter*/
+sudo cp node_exporter /usr/local/bin
+
+sudo cp nodeexporter.service /etc/systemd/system
 sudo systemctl enable systemdexporter
 sudo systemctl start systemdexporter
